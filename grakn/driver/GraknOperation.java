a92cac8a8dac9095e2299800cc2832f32bf15cf8
Rui Liu <lriuui0x0@gmail.com>
2020-12-22 20:44:18 +0000

    test simulation

--- grakn/driver/GraknOperation.java
+++ grakn/driver/GraknOperation.java
@@ -17,7 +17,6 @@
 
 package grakn.simulation.grakn.driver;
 
-import grabl.tracing.client.GrablTracingThreadStatic;
 import grakn.client.GraknClient;
 import grakn.client.answer.ConceptMap;
 import grakn.simulation.common.driver.LogWrapper;
@@ -31,9 +30,6 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.google.common.collect.Iterables.getOnlyElement;
-import static grabl.tracing.client.GrablTracingThreadStatic.traceOnThread;
-import static grakn.simulation.common.driver.TransactionalDbDriver.TracingLabel.EXECUTE;
-import static grakn.simulation.common.driver.TransactionalDbDriver.TracingLabel.SORTED_EXECUTE;
 
 public class GraknOperation extends TransactionalDbOperation {
 
@@ -70,41 +66,33 @@ public class GraknOperation extends TransactionalDbOperation {
     public <T> List<T> sortedExecute(GraqlGet query, String attributeName, Integer limit){
         throwIfClosed();
         log.query(tracker, query);
-        return trace(() -> {
-            Stream<T> answerStream = transaction.stream(query).get()
-                    .map(conceptMap -> (T) conceptMap.get(attributeName).asAttribute().value())
-                    .sorted();
-            if (limit != null) {
-                answerStream = answerStream.limit(limit);
-            }
-            return answerStream.collect(Collectors.toList());
-        }, SORTED_EXECUTE.getName());
+        Stream<T> answerStream = transaction.stream(query).get()
+                .map(conceptMap -> (T) conceptMap.get(attributeName).asAttribute().value())
+                .sorted();
+        if (limit != null) {
+            answerStream = answerStream.limit(limit);
+        }
+        return answerStream.collect(Collectors.toList());
     }
 
     public void execute(GraqlDelete query) {
         log.query(tracker, query);
-        trace(() -> transaction.execute(query).get(), EXECUTE.getName());
+        transaction.execute(query).get();
     }
 
     public List<ConceptMap> execute(GraqlInsert query) {
         log.query(tracker, query);
-        try (GrablTracingThreadStatic.ThreadTrace trace = traceOnThread(EXECUTE.getName())) {
-            return transaction.execute(query).get();
-        }
+        return transaction.execute(query).get();
     }
 
     public List<ConceptMap> execute(GraqlGet query) {
         log.query(tracker, query);
-        try (GrablTracingThreadStatic.ThreadTrace trace = traceOnThread(EXECUTE.getName())) {
-            return transaction.execute(query).get();
-        }
+        return transaction.execute(query).get();
     }
 
     public Number execute(GraqlGet.Aggregate query) {
         log.query(tracker, query);
-        try (GrablTracingThreadStatic.ThreadTrace trace = traceOnThread(EXECUTE.getName())) {
-            return getOnlyElement(transaction.execute(query).get()).number();
-        }
+        return getOnlyElement(transaction.execute(query).get()).number();
     }
 
     public Object getOnlyAttributeOfThing(ConceptMap answer, String varName, String attributeType) {
